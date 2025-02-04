aa6ea18e68dd52dd6e8ce5838321563229ec161f
andrejanev96 <>
2025-02-05 00:39:59 +0100

    added a like feature added the useEffect hook to only render once when liked/disliked

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
   const [hasLiked, setHasLiked] = useState(false);
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`)
+  }, [hasLiked])
+
+  useEffect(() => { 
+console.log('CARD RENDERED')
+  }, [])
+
   return (
-    <div className='card'>
-      <h2>{title}</h2>
+    <div className='card' onClick={ () => setCount(count + 1)}>
+      <h2>{title}
+        <br></br>
+        <span>👍 {count}</span>
+      </h2>
 
       <button onClick={() => { setHasLiked(!hasLiked) }}>
         {hasLiked ? '❤️' : '🤍'}
